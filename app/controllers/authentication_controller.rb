a43ba222a95ffc5c6a4c537904cc873dee8ac928
nadimsheikh <nadim.sheikh.07@gmail.com>
2019-05-01 17:13:40 +0530

    Access Token

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,10 +2,10 @@ class AuthenticationController < ApplicationController
     skip_before_action :authenticate_request,:verify_authenticity_token    
 
     def authenticate              
-        command = AuthenticateUser.call(params[:email], params[:password])
+        command = AuthenticateUser.call(params[:username], params[:password])
 
         if command.success?
-            render json: { auth_token: command.result }
+            render json: { access_token: command.result }
         else
             render json: { error: command.errors }, status: :unauthorized
         end
