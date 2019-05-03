8a78dcf278022db644429617d86233dbf3ba8939
nadimsheikh <nadim.sheikh.07@gmail.com>
2019-05-03 18:04:41 +0530

    set local cokkiew

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_request
     attr_reader :current_user
+    before_action :set_locale
 
     private
+
+  
+    def set_locale
+        I18n.locale = cookies[:lang] || I18n.default_locale
+    end
 
     def authenticate_request
         @current_user = AuthorizeApiRequest.call(request.headers).result
